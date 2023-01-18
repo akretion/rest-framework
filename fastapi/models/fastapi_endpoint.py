46a6e23b8c5af9b140169d3abbd526428ea6391c
Sébastien BEAU <sebastien.beau@akretion.com>
2023-01-18 23:13:57 +0100

    FIX typo

--- fastapi/models/fastapi_endpoint.py
+++ fastapi/models/fastapi_endpoint.py
@@ -220,10 +220,11 @@ class FastapiEndpoint(models.Model):
 
     def _get_app_exception_handlers(
         self,
-    ) -> Dict[
-        int | Type[Exception],
-        Callable[[Request, Exception], Union[Response, Awaitable[Response]]],
-    ]:
+    ):
+        #    -> Dict[
+        #    int | Type[Exception],
+        #    Callable[[Request, Exception], Union[Response, Awaitable[Response]]],
+        # ]:
         """Return a dict of exception handlers to register on the app
 
         The key is the exception class or status code to handle.
